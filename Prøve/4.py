770e4971d7cb25cfaa9fcaa8a85e170a6d2cdb8f
Johan Haga Mohn <47793383+JohanHagaMohn@users.noreply.github.com>
2020-03-03 09:48:51 +0100

    p

--- Prøve/4.py
+++ Prøve/4.py
@@ -1,14 +1,18 @@
+# Importerer pylab
 from pylab import *
 
+# Definerer funskjonene
 def NH3(T):
     return 0.00868 * T ** 2 - 1.66 * T + 87.4
 
 def NaCl(T):
     return 0.000295 * T ** 2 + 0.00554 * T + 35.7
     
+# Trekker dem fra hverandre for å forskjellen som en funksjon
 def likevekt(T):
     return NH3(T) - NaCl(T)
 
+# Implemeneterer halveringsmetoden
 def halver(f,a=10, b=90, tol=1E-8):
     m = (a + b) / 2
     while abs(f(m)) > tol:
@@ -19,8 +23,10 @@ def halver(f,a=10, b=90, tol=1E-8):
         m = (a + b) / 2
     return m
 
+# Finner når forskjellen er 0
 print(f'Løsningene er like ved {halver(likevekt)} grader C')
 
+# Plotter alle tre funksjonene i pylab (masse snacks)
 x = linspace(0, 90, 1000)
 y1 = NH3(x)
 y2 = NaCl(x)
--- Prøve/5.py
+++ Prøve/5.py
@@ -1,26 +1,35 @@
+# Importerer pylab
 from pylab import *
 
+# Konstanter
 k = 0.1
 N = 100000
 tidslengde = 60
 dt = 60 / (N - 1)
 sluttemperatur = 21
 
+# Startverdi(er)
 y0 = 75
 
+# Definerer arrayer
 t = zeros(N)
 tder = zeros(N - 1)
 tid = zeros(N)
 
+# Setter startverdi(er)
 t[0] = y0
 
+# Går igjennom løkka N antall ganger og legger in verdier ut ifra endringer
 for i in range(N - 1):
     tder[i] = -k * (t[i] - sluttemperatur)
     t[i + 1] = t[i] + tder[i] * dt
     tid[i + 1] = tid[i] + dt
+
+# Plotter (enda mer snacks)
 grid()
 xlabel("Tid (min)")
 ylabel("Temperatur (C)")
+# Bemerk at den deriverte er én mindre fordi den trenger to punkter for å finne endring. Derfor ekskluderer jeg siste tidsverdi
 plot(tid, t, "g", tid[:-1], tder, "r")
 legend(["Temperatur", "Temperaturendring"])
 show()
